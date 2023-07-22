832eaeba11d7be3737afa13b0a9ec52dadb65685
Eln Far <farzaliyevelnur00@gmail.com>
2023-07-22 23:43:05 +0400

    activity upsert

--- app/(mainpage)/track/page.tsx
+++ app/(mainpage)/track/page.tsx
@@ -2,33 +2,49 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { getUserSession } from "@/lib/auth"
 import { prisma } from "@/lib/prisma"
-import { Activity } from "@prisma/client"
+import { Activity, Client, Project } from "@prisma/client"
 import { revalidatePath } from "next/cache"
 import Duration from "./duration"
 import { ArrowRightIcon } from "@radix-ui/react-icons"
 import ActivityItem from "./activity-item"
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { Building2, ProjectorIcon } from "lucide-react"
 
 
 
 type NewActivityProps = {
     activity?:Activity | null
+    clients:Client[]
+    projects:Project[]
 }
 
-const NewActivity = ({activity}:NewActivityProps) => {
+const NewActivity = ({activity,clients,projects}:NewActivityProps) => {
 
-  async function startActivity(data:FormData){
+  async function upsertActivity(data:FormData){
     'use server'
 
     const user = await getUserSession()
     console.log(user);
     
-     await prisma.activity.create({
-      data: {
+      const client = data.get('client') as string
+      const project = data.get('project') as string
+
+     await prisma.activity.upsert({
+      where: {
+        id:data.get('id') as string,
+      },
+      create: {
         user: {connect:{id:user.id}},
         tenant: {connect:{id:user.tenant.id}},
         name: data.get('name') as string,
         startAt: new Date(),
-      
+       client: !!client ? {connect:{id:client}} : undefined,
+       project: !!project ? {connect:{id:project}} : undefined,
+      },
+      update: {
+        name: data.get('name') as string,
+        client: !!client ? {connect:{id:client}} : undefined,
+        project: !!project ? {connect:{id:project}} : undefined,
       }
     })
       revalidatePath('/track')
@@ -37,6 +53,9 @@ const NewActivity = ({activity}:NewActivityProps) => {
   async function stopActivity(data:FormData) {
     'use server'
 
+    const client = data.get('client') as string
+    const project = data.get('project') as string
+    
     await prisma.activity.update({
       where: {
         id:data.get('id') as string
@@ -44,6 +63,8 @@ const NewActivity = ({activity}:NewActivityProps) => {
       data: {
         endAt:new Date(),
         name: data.get('name') as string,
+        client: !!client ? {connect:{id:client}} : undefined,
+        project: !!project ? {connect:{id:project}} : undefined,
       }
     })
 
@@ -51,14 +72,59 @@ const NewActivity = ({activity}:NewActivityProps) => {
 
   }
 
+
+//   const clients = (await prisma.client.findMany({
+//     where: {
+//         tenantId:user.tenant.id
+//     }
+// })).map((client) => ({
+//     value:client.id,
+//     label:client.name
+// }))
+
+
   return (
     <div>
       <h2>What are you working on ?</h2>
 
-      <form action={activity ? stopActivity : startActivity}>
+      <form action={activity ? stopActivity : upsertActivity}>
         <div className="flex items-center space-x-4">
         <Input name="name" type="text" defaultValue={activity?.name || ''}/>
         <input type="hidden" name="id" defaultValue={activity?.id || ''}/>
+        <Select name="client">
+                        <SelectTrigger className="w-[180px]">
+                          <Building2 size={32}/>
+                            <SelectValue placeholder="Assign a client" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectGroup>
+                            <SelectLabel>Client</SelectLabel>
+                            <SelectItem value="">None</SelectItem>
+                                {clients.map((client) => (
+                                      <SelectItem value={client.id} key={client.id}>{client.name}</SelectItem>
+                                ))}
+                            </SelectGroup>
+                        </SelectContent>
+                        </Select>
+
+                        <Select name="project">
+                        <SelectTrigger className="w-[180px]">
+                          <ProjectorIcon size={32}/>
+                            <SelectValue placeholder="Assign a Project" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectGroup>
+                            <SelectLabel>Project</SelectLabel>
+                            <SelectItem value="">None</SelectItem>
+                                {projects.map((project) => (
+                                      <SelectItem value={project.id} key={project.id}>{project.name}</SelectItem>
+                                ))}
+                            </SelectGroup>
+                        </SelectContent>
+                        </Select>
+
+
+
         {activity && ( <Duration startAt={activity.startAt}/>)} 
         <Button type="submit" value="submit">{activity ? 'Stop':'Start'}</Button>
         </div>
@@ -89,6 +155,18 @@ export default async function TrackTimePage() {
 
   const user = await getUserSession()
 
+  const clients= await prisma.client.findMany({
+    where: {
+      tenantId:user.tenant.id
+    }
+  })
+  
+  const projects = await prisma.project.findMany({
+    where: {
+      tenantId:user.tenant.id
+    }
+  })
+  
   
   
   const currentActivity = await prisma.activity.findFirst({
@@ -128,7 +206,7 @@ export default async function TrackTimePage() {
   })
   return (
     <main  className="container py-4">
-      <NewActivity activity={currentActivity}/>
+      <NewActivity activity={currentActivity} projects={projects} clients={clients}/>
       <DailyActivities activities={dailyActivities}/>
     </main>
   )
