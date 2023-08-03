69a42c13c2c66a21594c65c6476b7fe738ee0b17
Eln Far <farzaliyevelnur00@gmail.com>
2023-08-04 02:57:58 +0400

    removed account

--- app/api/account/route.ts
+++ app/api/account/route.ts
@@ -1,18 +1,18 @@
-import { prisma } from '@/lib/prisma'
-import { NextResponse } from 'next/server'
+// import { prisma } from '@/lib/prisma'
+// import { NextResponse } from 'next/server'
 
-export async function GET() {
-  const res = await prisma.tenant.updateMany({
-    where: {
-      expirationDate: {
-        lte: new Date()
-      }
-    },
-    data: {
-      plan: 'FREE'
-    }
-  })
+// export async function GET() {
+//   const res = await prisma.tenant.updateMany({
+//     where: {
+//       expirationDate: {
+//         lte: new Date()
+//       }
+//     },
+//     data: {
+//       plan: 'FREE'
+//     }
+//   })
 
-  return NextResponse.json({ updated: res.count })
-}
+//   return NextResponse.json({ updated: res.count })
+// }
 
